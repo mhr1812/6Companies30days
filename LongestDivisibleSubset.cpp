fac3441176452129a01271febfb3e24527c3ae46
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-01-04 00:03:02 +0530

    Add files via upload

---
+++ LongestDivisibleSubset.cpp
@@ -0,0 +1,31 @@
+//Sort + Longest Divisible Subsequence
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,1),hash(n);
+        int maxi = 1;
+        int lastIndex = 0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            hash[i] = i;
+            for(int prev = 0;prev<i;prev++){
+                if(nums[i]%nums[prev]==0 and dp[i]<dp[prev]+1){
+                    dp[i]=dp[prev]+1;
+                    hash[i] = prev;
+                }
+            }
+            if(dp[i]>maxi){
+                maxi = dp[i];
+                lastIndex = i;
+            }
+        }
+        vector<int> ans;
+        ans.push_back(nums[lastIndex]);
+        while(hash[lastIndex]!=lastIndex){
+            lastIndex = hash[lastIndex];
+            ans.push_back(nums[lastIndex]);
+        }
+        return ans;
+    }
+};
